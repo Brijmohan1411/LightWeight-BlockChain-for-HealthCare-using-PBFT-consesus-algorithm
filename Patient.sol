93c6c415982c7196baf1823f3260da3977002234
Brijmohan1411 <71484166+Brijmohan1411@users.noreply.github.com>
2022-07-13 18:34:32 +0530

    commit2

--- Patient.sol
+++ Patient.sol
@@ -1,27 +1,28 @@
 pragma solidity ^0.8.0;
-
-contract Patient{
-
-    struct patient{
-        string name;
-        bool isUser;
-        address id;
-        uint adharNo;
-        string state;
-    }
-    mapping( address => patient ) patients;
-
-    function register(string memory _name,address _id,uint _adhar,string memory _state)public {
-        require(patients[_id].isUser==false,"already registered");
-        patients[_id]=patient(_name,_id,true,_adhar,_state);
+import "../../BCM/MANAGER/Manager.sol";
+import "../../HBCM/COLLECTOR/Collector.sol";
+contract Patient is Collector,Manager{
+    modifier onlyPatitent(string memory adhar){
+        bytes32 validate=callKeccak256(adhar);
+        require(isPatient[validate],"sry you are not user");
     }
 
-    function updateRequest(address _hId, string memory _disease,address _doctorId){
-        require(patients[msg.sender].isUser==true,"sorry, you are not registered yet");
-        //here transaction is added to trMap(trColl) and will be waiting to be included in a block;
-        trColl[trNo++]=tr(msg.sender,_hId,_disease,_doctorId); 
+
+   event userMsg(string res);
+   event userReportEvent();
+    function requestReport(string memory adhar) public onlyPatient(adhar){
+        bytes32 id=callKeccak256(adhar);
+        require(!userStatus[id],"Already Requested");
+        userRequest[patientRequestNo]=id;
+        patientRequestNo++;
+        userStatus[id]=true;
+        emit userMsg("successFul done");
     }
 
-   
+    function checkReport(string memory adhar) public onlyPatient(adhar){
+        bytes32 id=callKeccak256(adhar);
+        require(!userStatus[id],"in queue wait for approval");
+        emit userReportEvent();
 
+    }
 }
