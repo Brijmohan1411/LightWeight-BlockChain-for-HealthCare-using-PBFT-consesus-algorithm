6f81c782e4558514f0b7d82514d69a182005ada0
Brijmohan1411 <71484166+Brijmohan1411@users.noreply.github.com>
2022-07-13 18:32:29 +0530

    Update BCM.sol

--- BCM.sol
+++ BCM.sol
@@ -1,71 +1,72 @@
 pragma solidity ^0.8.0;
-
-contract BCM{
-
-    struct bcm{
-
-        string name;
-        address id;
-        string state;
-        bool isBCM;
-       // uint totalResponses;
-       // uint totalCorrectResponses;
+import "../../HBCM/COLLECTOR/Sender.sol";
+import "../../HBCM/COLLECTOR/Collector.sol";
+import "../CONSENSUS/consensus.sol";
+contract Manager is Sender,Consensus,Collector{
+    modifier onlyManager(){
+        require(msg.sender==manager,"Only Manager can access");
+        _;
     }
-    mapping(address => bcm ) bcmColl;
-
-    uint[5] responseArray;
-    mapping(uint=>responseArray) responseColl;
-   // mapping(uint=>address) resToResponserMap;
-    uint totalRes;
-
-
-    function getNextBlock() public view {
+    event approvePatientEvent(String msg);
+    mapping(uint=>bytes32) userRequest;
+    mapping(bytes32=>bool) userStatus;
+    struct patientMediacalRecord{
 
     }
-    function response(uint res1,uint res2,uint res3,uint res4,uint res5)public onlyBCM(){
-          require(res1==0||res1==1||
-                  res2==0||res2==1||
-                  res3==0||res3==1||
-                  res4==0||res4==1||
-                  res5==0||res5==1  ,"incorrect input, u can enter only 0 0r 1 as input");
-                  uint[5] array;
-                
-                  array[0]=res1;
-                  array[1]=res2;
-                  array[2]=res3;
-                  array[3]=res3;
-                  array[4]=res4;
-                     
+    mapping(bytes32=>mapping(uint=>patientMediacalRecord)) record;
+    mapping(bytes32=>patientMediacalRecord) forUser;
+    function addpatientRecord() public onlyManager{
 
-                   responseColl[totalRes++]=array;
     }
-
-    function updateLedgerVTorIVT() onlyHBCM {
-        for(uint i=0;i<5;i++){
-            int cntV=0;     //count of valid responses
-            for(uint res=0;res<totalRes;res++){
-              if(responseColl[res].[i] == 1){
-                  cntV++;
-                  responseColl[res].[i] == 0; //preparation doing for next block;
-              }
-          }
-          uint v=0;     //v=1 for valid and 0 for invalid
-          
-          if((cntV/totalRes)>(2/3)){
-              v=1;
-          }
-         tr t=mapLT[currBlock].[i];         // tr is transaction of which validation is being checked
-         address uAdd=mapLT[currBlock].[i].id;
-         mapLVT[uAdd].push(ledger(v, t.disease,t.hId,t.doctorId));
-
-       
-
-
+    function approvaPatientStatus() public onlyManager{
+        if(patientApproveNo==patientRequestNo){
+            patientApproveNo=0;
+            patientRequestNo=0;
+            emit approvaPatientStatus("All user request approved");
         }
+        else{
+            bytes32 id=userRequest[patientRequestNo];
+            totalReport[id];
+            require(totalReport[id]>0,"Not Checked yet");
+            mapping(uint=>patientMediacalRecord) res=record[id];
+            patientMediacalRecord storage r=res[totalReport[id]-1];
+            forUser[id]=r;
+            patientApproveNo++;
+            emit approvaPatientStatus("successFul Approve");
+        }
+    }
 
-         //preparation for next block validation;
-       totalRes=0;
-       
-     }
+   function gethashFromBCM()public onlyManager returns(bytes32) {
+            int currTransactionNo=executedTrNo;
+            transaction storage updatingInfo = transactionCollection[currTransactionNo];
+            string memory str_id=updatingInfo.id;
+            string memory str_disease=updatingInfo.disease;
+            bytes32 hash1=keccak256(abi.encode(str_id, str_disease));
+            return hash1;
+    }
 
+event giveResponseToTrNo(uint executedTrNo);
+
+function mineBlock()public onlyCollector{
+       if(trNo>=executedTrNo+blocksize){
+      for(int i=0;i<blocksize;i++){
+          emit giveResponseToTrNo(executedTrNo);
+            bytes32 [10]responses;
+            for(int resno=0;resno<10;resno++){
+                
+               responses[resno] = gethashFromBCM();
+            }
+            bytes32 val = consesusPBFT(responses);
+            if(val==-1){
+
+            }else{
+                //data update
+            }
+            executedTrNo++;
+      }
+      trNo=trNo-blocksize;
+    }
+}  
+
+    
 }
